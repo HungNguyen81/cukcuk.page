fcee2795a23ef1de58493ee00bcf078afc4a0039
hungnguyen81 <nguyenngochung.ncth@gmail.com>
2021-07-24 23:39:12 +0700

    ok

--- 1.UI/js/table.js
+++ 1.UI/js/table.js
@@ -8,7 +8,7 @@ function UpdateEmployeeTable() {
     FillTableData(pageSize, 1);
     ChangeCurrentPageLabel(pageSize, 1);
     UpdatePagingBar();
-    ToggleDeleteButton('hidden');
+    // ToggleDeleteButton('hidden');
 }
 
 function BackupEmployees() {
